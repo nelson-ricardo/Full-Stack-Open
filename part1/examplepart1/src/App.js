6159b27319768dfe4005bc0b4f163c19019b2a48
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-07-06 12:01:14 -0600

    add example exercise

---
+++ part1/examplepart1/src/App.js
@@ -0,0 +1,25 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age}!
+      </p>
+    </div>
+  );
+};
+
+function App() {
+  const name = "Nelson";
+  const age = 20;
+
+  return (
+    <div className="App">
+      <h1>Greetings</h1>
+      <Hello name={name} age={age} />
+      <Hello name="Sam" age={age + 10} />
+      <Hello name="John" age={age * 2} />
+    </div>
+  );
+}
+
+export default App;
