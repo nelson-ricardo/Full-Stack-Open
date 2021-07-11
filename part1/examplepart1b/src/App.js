dbc46034bbd7f7fc8db941d581818eaedbccd3b1
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-07-11 11:28:53 -0600

    finished second example app

--- part1/examplepart1b/src/App.js
+++ part1/examplepart1b/src/App.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import {useState} from 'react';
 
+const History = ({allClicks}) => {
+  if(allClicks.length === 0) {
+    return(
+      <div>
+        <p>The app is used by pressing the buttons</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Button press history: {allClicks.join(' ')}
+      </div>
+    )
+  }
+}
+
+const Button = ({text, handleClick}) => {
+  return(
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  });
+  const [allClicks, setAllClicks] = useState([]);
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    };
+
+    setAllClicks(allClicks.concat("L"));
+    setClicks(newClicks);
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    };
+
+    setAllClicks(allClicks.concat("R"));
+    setClicks(newClicks);
+  }
 
   return (
     <div>
-      {counter}
-      <button onClick={() => setCounter(counter + 1)}>
-        increase
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      {clicks.left}
+      <Button text="Increase Left" handleClick={handleLeftClick}/>
+      <Button text="Increase Right" handleClick={handleRightClick}/>
+      {clicks.right}
+      <History allClicks={allClicks} />
     </div>
-
   );
-};
+}
 
 export default App;
